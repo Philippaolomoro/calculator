4497a62608af22a0d5b51a471d3606758ca5f2b3
Olomoro Philippa <olomoro.philippa@gmail.com>
2020-04-09 22:34:22 +0100

    added new

---
+++ main.js
@@ -0,0 +1,25 @@
+let currentInput = 0;
+const pendingValue;
+const evalEmptyArray = [];
+
+//using selectors to target our DOM elements
+const button = document.getElementsByClassName('.number-buttons')
+const clear = document.getElementById('clear')
+const screenInput = document.getElementById('screen-input')
+const backArrow = document.getElementById('back-arrow')
+const operators = document.getElementsByClassName('.operators')
+
+//adding event listeners to our buttons and operators to perform
+for (var i = 0; i<button.length; i++) {
+    button[i].addEventListener('click', function () {
+ 
+    });
+}
+
+for (var i = 0; i<operators.length; i++) {
+    operators[i].addEventListener('click', function () {
+
+    });
+}
+
+
