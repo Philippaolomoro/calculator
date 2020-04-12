d08db8824c5143c437dc903e03d6e812c7c6e06f
Olomoro Philippa <olomoro.philippa@gmail.com>
2020-04-12 01:35:38 +0100

    changed the style of the calculator. also started working on the javascript

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-let currentInput = 0;
-const pendingValue;
-const evalEmptyArray = [];
+let numberButtons = document.querySelector(".number-button");
+let screenInput = document.querySelector("#screen-input");
+let clear = document.querySelector("#clear");
+let backArrow = document.querySelector("#back-arrow");
+let operators = document.querySelector(".operators");
 
-//using selectors to target our DOM elements
-const button = document.getElementsByClassName('.number-buttons')
-const clear = document.getElementById('clear')
-const screenInput = document.getElementById('screen-input')
-const backArrow = document.getElementById('back-arrow')
-const operators = document.getElementsByClassName('.operators')
+let screenDisplay = 0;
+let evalInput = [];
+let pendingVal;
 
-//adding event listeners to our buttons and operators to perform
-for (var i = 0; i<button.length; i++) {
-    button[i].addEventListener('click', function () {
- 
+for(let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener("click", () => {
+        
     });
 }
 
-for (var i = 0; i<operators.length; i++) {
-    operators[i].addEventListener('click', function () {
+for(let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click", () => {
 
     });
 }
 
+clear.addEventListener("click", ()=> {
 
+});
+
+backArrow.addEventListener("click", ()=> {
+
+});
