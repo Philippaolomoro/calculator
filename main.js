70ecaabfed5e9804ca0deff0ea1e6886a33d2056
Philippa Olomoro <olomoro.philippa@gmail.com>
2020-04-20 00:36:24 +0100

    made basic calculations possible

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let screenDisplay = document.querySelector('#screen-display');
 let numberButtons = document.querySelectorAll(".number-buttons");
 let clearButton = document.querySelector('#clear');
+let operatorButtons = document.querySelectorAll('.operators');
+let backArrowButton = document.querySelector("#back-arrow");
 
 let screenInput = '0';
+let pendingValue;
+let evalInput = [];
 
 let currentScreenDisplay = (e) => {
     let buttonText = e.target.innerText;
@@ -15,10 +19,54 @@ let currentScreenDisplay = (e) => {
 
 for(let i = 0; i < numberButtons.length; i++){
     numberButtons[i].addEventListener("click", currentScreenDisplay);
-    console.log("yes");
 }
 
 clearButton.addEventListener('click', ()=> {
     screenInput = '0'
     screenDisplay.innerText = screenInput
-})
+});
+
+let operations = (e) =>{
+    let calculations = e.target.innerText;
+    switch(calculations){
+        case '÷':
+            pendingValue = screenInput;
+            screenInput = " "
+            screenDisplay.innerText = screenInput;
+            evalInput.push(pendingValue, "/");
+            break;
+        case 'x':
+            pendingValue = screenInput;
+            screenInput = " ";
+            screenDisplay.innerText = screenInput;
+            evalInput.push(pendingValue, "*");
+            break;
+        case '-':
+            pendingValue = screenInput;
+            screenInput = " ";
+            screenDisplay.innerText = screenInput;
+            evalInput.push(pendingValue, "-");
+            break;
+        case '+':
+            pendingValue = screenInput;
+            screenInput = " ";
+            screenDisplay.innerText = screenInput;
+            evalInput.push(pendingValue, "+");
+            break;
+        case "=":
+            evalInput.push(screenInput);
+            let yes = eval(evalInput.join(' '));
+            screenInput = yes + '';
+            screenDisplay.innerText = screenInput;
+            evalInput = [];
+            break;
+        default:
+            break;
+    }
+}
+
+for(let i= 0; i<operatorButtons.length; i++){
+    operatorButtons[i].addEventListener('click', operations);
+}
+
+
